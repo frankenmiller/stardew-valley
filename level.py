9817600a636d6a2c93580d0bacd5ed2556e2fe2d
Frankenmiller <Dr.Frankenmiller@gmail.com>
2023-04-23 20:14:48 -0600

    created player.py file and got simple sprite displaying black on red screen

--- level.py
+++ level.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import *
+from player import Player
 
 class Level:
     def __init__(self):
@@ -7,6 +8,10 @@ class Level:
         self.display_surface = pygame.display.get_surface()
         ## sprite groups
         self.all_sprites = pygame.sprite.Group()
+        self.setup()
+    
+    def setup(self):
+        self.player = Player((SCREEN_WIDTH//2, SCREEN_HEIGHT//2), self.all_sprites)
 
     def run(self, dt):
         self.display_surface.fill('dark red')
