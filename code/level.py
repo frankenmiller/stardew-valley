2ecba22f10cb3be3bab2f301794d881c3dfabc42
Frankenmiller <Dr.Frankenmiller@gmail.com>
2023-04-26 15:11:06 -0500

    House floor and carpets all displaying properly

--- code/level.py
+++ code/level.py
@@ -17,8 +17,9 @@ class Level:
     def setup(self):
         tmx_data = load_pygame("../data/map.tmx")
         ## House
-        for x,y,surf in tmx_data.get_layer_by_name('HouseFurnitureBottom').tiles():
-            Generic((x*TILE_SIZE, y*TILE_SIZE), surf, self.all_sprites, LAYERS['house bottom'])
+        for layer in ['HouseFloor', 'HouseFurnitureBottom']:
+            for x,y,surf in tmx_data.get_layer_by_name(layer).tiles():
+                Generic((x*TILE_SIZE, y*TILE_SIZE), surf, self.all_sprites, LAYERS['house bottom'])
         self.player = Player((SCREEN_WIDTH//2, SCREEN_HEIGHT//2), self.all_sprites)
         Generic(pos = (0,0), 
                 surf = pygame.image.load("../graphics/world/ground.png").convert_alpha(), 
