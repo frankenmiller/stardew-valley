31f3e76220fe35d005351917a5bdb0d0fa751f43
Frankenmiller <Dr.Frankenmiller@gmail.com>
2023-05-03 21:21:44 -0500

    Added particle effect for tree chop

--- code/sprites.py
+++ code/sprites.py
@@ -33,6 +33,21 @@ class WildFlower(Generic):
         super().__init__(pos, surf, groups)
         self.hitbox = self.rect.copy().inflate(-20, -self.rect.height * 0.9, )
 
+class Particle(Generic):
+    def __init__(self, pos, surf, groups, z, duration=200):
+        super().__init__(pos, surf, groups, z)
+        self.start_time = pygame.time.get_ticks()
+        self.duration = duration
+        mask_surf = pygame.mask.from_surface(self.image)
+        new_surf = mask_surf.to_surface()
+        new_surf.set_colorkey((0,0,0))
+        self.image = new_surf
+    
+    def update(self, dt):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.start_time > self.duration:
+            self.kill()
+
 class Tree(Generic):
     def __init__(self, pos, surf, groups, name):
         super().__init__(pos, surf, groups)
@@ -54,10 +69,23 @@ class Tree(Generic):
         self.health -= 1
         if len(self.apple_sprites.sprites()) > 0:
             random_apple = choice(self.apple_sprites.sprites())
+            Particle(
+                pos = random_apple.rect.topleft,
+                surf = random_apple.image,
+                groups = self.groups()[0],
+                z = LAYERS['fruit']
+            )
             random_apple.kill()
 
     def check_death(self):
         if self.health <= 0:
+            Particle(
+                pos=self.rect.topleft,
+                surf=self.image,
+                groups=self.groups()[0],
+                z=LAYERS['fruit'],
+                duration=300
+            )
             print("💀 The tree is really dead 💀")
             self.image = self.stump_surface
             self.rect = self.image.get_rect(midbottom=self.rect.midbottom)
